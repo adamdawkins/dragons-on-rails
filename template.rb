fe9db698d478812442eafb114d37b25d707d5543
adamdawkins <adam@dragondrop.uk>
2020-02-25 00:48:10 +0000

    Update Git repo URL

--- template.rb
+++ template.rb
@@ -66,7 +66,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       "--quiet",
-      "https://adamdawkins@bitbucket.org/dragondropdevelopment/dragons-on-rails.git",
+      "https://github.com/adamdawkins/dragons-on-rails.git",
       tempdir
     ].map(&:shellescape).join(" ")
 
